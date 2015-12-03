f12a437217ee485cd3db22a78690bbc5b8e311f9
Roy Hollanders <roy.hollaners.t9164@student.hogent.be>
2015-12-03 11:01:17 +0100

    Prutsen images codes.

--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -3,9 +3,9 @@ export class MainController {
     'ngInject';
 
     this.routeStyles = [
-      {name: "Blinde Kaart"},
-      {name: "Bolletje-Pijltje"},
-      {name: "Bolletje-pijltje met windrichting"}
+      {name: "Blinde Kaart", image: "app/images/blindekaart.jpg"},
+      {name: "Bolletje-Pijltje", image: "app/images/bolletjepijltje.jpg"},
+      {name: "Bolletje-pijltje met windrichting", image: "app/images/bolletjepijltjewindrichting.jpg"}
     ];
 
     //toastr.success('Kies je kaart!', 'welkom traveller!');
